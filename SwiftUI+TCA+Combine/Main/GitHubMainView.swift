468e8ef45b185b658494580ff9a1e198f33eb27a
bgjeon <bgjeon@lotte.net>
2025-03-25 15:35:54 +0900

    Indentation 4로 변경

--- SwiftUI+TCA+Combine/Main/GitHubMainView.swift
+++ SwiftUI+TCA+Combine/Main/GitHubMainView.swift
@@ -8,57 +8,59 @@
 import SwiftUI
 
 struct GitHubMainView: View {
-   private let buttonWidth = UIScreen.main.bounds.width / 2
-   private let buttonHeight = 50.0
-   @State private var barPoint = CGPoint(x: 0, y: 0)
-   @State var searchText = ""
-   
-   var body: some View {
-      NavigationView {
-         VStack() {
-            Text("Github Stars")
-               .frame(alignment: .leading)
-            
-            VStack {
-               HStack {
-                  Button {
-                     withAnimation {
-                        barPoint.x = 0
-                     }
-                  } label: {
-                     Text("API")
-                        .frame(width: buttonWidth, height: buttonHeight)
+    private let buttonWidth = UIScreen.main.bounds.width / 2
+    private let buttonHeight = 50.0
+    @State private var barPoint = CGPoint(x: 0, y: 0)
+    @State var searchText = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Github Stars")
+                    .frame(alignment: .leading)
+                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 10, trailing: 15))
+                
+                VStack {
+                    HStack {
+                        Button {
+                            withAnimation {
+                                barPoint.x = 0
+                            }
+                        } label: {
+                            Text("API")
+                                .frame(width: buttonWidth, height: buttonHeight)
+                                .border(Color.black, width: 1)
+                                .foregroundStyle(Color.black)
+                        }
+                        
+                        Button {
+                            withAnimation {
+                                barPoint.x = buttonWidth
+                            }
+                        } label: {
+                            Text("Local")
+                                .frame(width: buttonWidth, height: buttonHeight)
+                                .border(Color.black, width: 1)
+                                .foregroundStyle(Color.black)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+                    
+                    Rectangle()
+                        .position(barPoint)
+                        .frame(width: buttonWidth, height: 2)
+                        .foregroundColor(.blue)
+                    
+                    TextField("검색어를 입력해주세요.", text: $searchText)
+                        .padding()
                         .border(Color.black, width: 1)
-                        .foregroundStyle(Color.black)
-                  }
-                  
-                  Button {
-                     withAnimation {
-                        barPoint.x = buttonWidth
-                     }
-                  } label: {
-                     Text("Local")
-                        .frame(width: buttonWidth, height: buttonHeight)
-                        .border(Color.black, width: 1)
-                        .foregroundStyle(Color.black)
-                  }
-               }
-               .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-               
-               Rectangle()
-                  .position(barPoint)
-                  .frame(width: buttonWidth, height: 2)
-                  .foregroundColor(.blue)
-               
-               TextField("검색어를 입력해주세요.", text: $searchText)
-                  .padding()
-                  .border(Color.black, width: 1)
+                }
             }
-         }
-      }
-   }
+//            .padding(15)
+        }
+    }
 }
 
 #Preview {
-   GitHubMainView()
+    GitHubMainView()
 }
--- SwiftUI+TCA+Combine/Profile/View/ProfileView.swift
+++ SwiftUI+TCA+Combine/Profile/View/ProfileView.swift
@@ -8,61 +8,68 @@
 import SwiftUI
 import Kingfisher
 
+struct Profile {
+    let initial: String?
+    let profileImageURL: String?
+    let userName = ""
+    @State var isSelected = false
+}
+
 struct ProfileView: View {
-   private var imageSize = 60.0
-   private var starSzie = 30.0
-   var initial = "A"
-   var profileImageUrl: String? = nil
-   var userName = "김땡땡"
-   @State var isSelected = false
-   
-   var body: some View {
-      VStack(alignment: .leading, spacing: 0) {
-         Text(initial)
-            .font(.system(size: 17.0))
-            .foregroundColor(Color.black)
-            .bold()
-            .padding(EdgeInsets(top: 0, leading: 15, bottom: 10, trailing: 15))
-            .frame(maxWidth: Screen.width, alignment: .leading)
-            .border(Color.red, width: 1)
-         
-         HStack(alignment: .center, spacing: 0) {
-            if let imageUrl = profileImageUrl {
-               KFImage(URL(string: imageUrl))
-                  .resizable()
-                  .scaledToFill()
-                  .frame(width: imageSize, height: imageSize)
-                  .clipShape(Circle())
-            } else {
-               Image(systemName: "person.circle.fill")
-                  .resizable()
-                  .frame(width: imageSize, height: imageSize)
-                  .foregroundStyle(Color(.systemGray4))
+    private var imageSize = 60.0
+    private var starSzie = 30.0
+    var initial = "A"
+    var profileImageUrl: String? = nil
+    var userName = "김땡땡"
+    @State var isSelected = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(initial)
+                .font(.system(size: 17.0))
+                .foregroundColor(Color.black)
+                .bold()
+                .padding(EdgeInsets(top: 0, leading: 15, bottom: 10, trailing: 15))
+                .frame(maxWidth: Screen.width, alignment: .leading)
+                .border(Color.red, width: 1)
+            
+            HStack(alignment: .center, spacing: 0) {
+                if let imageUrl = profileImageUrl {
+                    KFImage(URL(string: imageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: imageSize, height: imageSize)
+                        .clipShape(Circle())
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: imageSize, height: imageSize)
+                        .foregroundStyle(Color(.systemGray4))
+                }
+                
+                Text(userName)
+                    .font(.system(size: 17.0))
+                    .foregroundColor(Color.black)
+                    .lineLimit(2)
+                    .padding(EdgeInsets(top: 0, leading: 19, bottom: 0, trailing: 15))
+                    .frame(minWidth: 280, alignment: .leading)
+                
+                Image(systemName: isSelected ? "star.fill" : "star")
+                    .foregroundStyle(Color.blue)
+                    .frame(width: starSzie, height: starSzie)
+                    .onTapGesture {
+                        withAnimation {
+                            isSelected.toggle()
+                        }
+                    }
             }
-            
-            Text(userName)
-               .font(.system(size: 17.0))
-               .foregroundColor(Color.black)
-               .lineLimit(2)
-               .padding(EdgeInsets(top: 0, leading: 19, bottom: 0, trailing: 15))
-               .frame(minWidth: 280, alignment: .leading)
-            
-            Image(systemName: isSelected ? "star.fill" : "star")
-               .foregroundStyle(Color.blue)
-               .frame(width: starSzie, height: starSzie)
-               .onTapGesture {
-                  withAnimation {
-                     isSelected.toggle()
-                  }
-               }
-         }
-         .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-         .frame(width: Screen.width, alignment: .leading)
-         .border(Color.blue, width: 1)
-      }
-      .frame(width: Screen.width, height: 105.0)
-      .border(Color.black, width: 1)
-   }
+            .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+            .frame(width: Screen.width, alignment: .leading)
+            .border(Color.blue, width: 1)
+        }
+        .frame(width: Screen.width, height: 105.0)
+        .border(Color.black, width: 1)
+    }
 }
 
 #Preview {
