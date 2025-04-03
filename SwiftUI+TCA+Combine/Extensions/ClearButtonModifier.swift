13476267ddde69ce87f84bfc414b87b0fe0111fd
Bonggil Jeon <jeonbongkill@naver.com>
2025-04-03 15:15:16 +0900

    TextField Modifier 추가

---
+++ SwiftUI+TCA+Combine/Extensions/ClearButtonModifier.swift
@@ -0,0 +1,35 @@
+//
+//  ClearButtonModifier.swift
+//  SwiftUI+TCA+Combine
+//
+//  Created by ec-jbg on 4/3/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct ClearButtonModifier: ViewModifier {
+  @Binding var text: String
+  var action: () -> Void
+  
+  func body(content: Content) -> some View {
+    HStack {
+      content
+      if !text.isEmpty {
+        Button {
+          text = ""
+        } label: {
+          Image(systemName: "multiply.circle.fill")
+            .foregroundColor(.gray)
+        }
+        .padding(.trailing, 8)
+      }
+    }
+  }
+}
+
+extension View {
+  func clearButton(text: Binding<String>, action: @escaping () -> Void) -> some View {
+    modifier(ClearButtonModifier(text: text, action: action))
+  }
+}
