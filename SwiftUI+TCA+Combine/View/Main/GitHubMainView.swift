a697b158bc186617e3f05a3b9496df6ebc3146d7
jeonbonggil <jeonbongkill@gmail.com>
2025-05-05 22:39:01 +0900

    refactor: deprecated 코드 제거

--- SwiftUI+TCA+Combine/View/Main/GitHubMainView.swift
+++ SwiftUI+TCA+Combine/View/Main/GitHubMainView.swift
@@ -15,88 +15,86 @@ struct GitHubMainView: View {
   @State private var currentTab: MenuTab = .api
   
   var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      VStack(alignment: .leading, spacing: 0) {
-        Text("Github Stars")
-          .font(.title)
-          .frame(width: Screen.width, alignment: .leading)
-          .padding(EdgeInsets(top: 10, leading: 25, bottom: 20, trailing: 0))
-        
-        VStack(spacing: 0) {
-          ZStack(alignment: .bottom) {
-            HStack(spacing: 0) {
-              ForEach(MenuTab.allCases, id: \.self) { tab in
-                Button {
-                  currentTab = tab
-                  viewStore.send(.selectedTabDidChange(tab))
-                  withAnimation {
-                    barPoint.x = tab.xOffset
-                  }
-                } label: {
-                  Text(tab.title)
-                    .frame(width: tab.menuWidth, height: tab.menuHeight)
-                    .border(.black, width: 1)
-                    .foregroundStyle(.black)
+    VStack(alignment: .leading, spacing: 0) {
+      Text("Github Stars")
+        .font(.title)
+        .frame(width: Screen.width, alignment: .leading)
+        .padding(EdgeInsets(top: 10, leading: 25, bottom: 20, trailing: 0))
+      
+      VStack(spacing: 0) {
+        ZStack(alignment: .bottom) {
+          HStack(spacing: 0) {
+            ForEach(MenuTab.allCases, id: \.self) { tab in
+              Button {
+                currentTab = tab
+                store.send(.selectedTabDidChange(tab))
+                withAnimation {
+                  barPoint.x = tab.xOffset
                 }
+              } label: {
+                Text(tab.title)
+                  .frame(width: tab.menuWidth, height: tab.menuHeight)
+                  .border(.black, width: 1)
+                  .foregroundStyle(.black)
               }
             }
-            .padding(.horizontal, 15)
-            
-            Rectangle()
-              .position(barPoint)
-              .frame(width: MenuTab.api.menuWidth, height: 2)
-              .foregroundColor(.blue)
           }
+          .padding(.horizontal, 15)
           
-          TextField("검색어를 입력해주세요.", text: $localSearchText)
-            .clearButton(text: $localSearchText, action: {
-              localSearchText = ""
-              viewStore.send(.clearSearchText)
-            })
-            .submitLabel(.search)
-            .onSubmit {
-              // Keyboard 검색 Button 클릭 시 검색
-              viewStore.send(.searchTextDidChange(localSearchText))
-              UIApplication.shared.endEditing()
-            }
-            .padding()
-            .border(.black, width: 1)
-          
-          ScrollViewReader { proxy in
-            WithPerceptionTracking {
-              ScrollView {
-                LazyVStack(spacing: 0) {
-                  ForEachStore(
-                    store.scope(state: \.profileFeatures, action: \.profileFeatures)
-                  ) { profileStore in
-                    ProfileView(store: profileStore)
-                      .onAppear {
-                        // TODO: - 실행 방법을 모르겠음(viewStore, store 둘 다 싪패)
-//                        store.send(.loadMore(index))  // Infinite Scrolling
-                      }
-                  }
+          Rectangle()
+            .position(barPoint)
+            .frame(width: MenuTab.api.menuWidth, height: 2)
+            .foregroundColor(.blue)
+        }
+        
+        TextField("검색어를 입력해주세요.", text: $localSearchText)
+          .clearButton(text: $localSearchText, action: {
+            localSearchText = ""
+            store.send(.clearSearchText)
+          })
+          .submitLabel(.search)
+          .onSubmit {
+            // Keyboard 검색 Button 클릭 시 검색
+            store.send(.searchTextDidChange(localSearchText))
+            UIApplication.shared.endEditing()
+          }
+          .padding()
+          .border(.black, width: 1)
+        
+        ScrollViewReader { proxy in
+          WithPerceptionTracking {
+            ScrollView {
+              LazyVStack(spacing: 0) {
+                ForEach(
+                  store.scope(state: \.profileFeatures, action: \.profileFeatures)
+                ) { profileStore in
+                  ProfileView(store: profileStore)
+                    .onAppear {
+                      // TODO: - 실행 방법을 모르겠음
+                      // store.send(.loadMore(index))  // Infinite Scrolling
+                    }
                 }
               }
-              .simultaneousGesture(DragGesture().onChanged { _ in
-                // ScrollView에서 Drag 시 Keyboard 내리기
-                UIApplication.shared.endEditing()
-              })
-              .onChange(of: viewStore.searchText) { _ in
-                proxy.scrollTo(0, anchor: .top) // 스크롤 최상단으로 이동
-              }
-              .onChange(of: viewStore.selectedTab) { _ in
-                proxy.scrollTo(0, anchor: .top) // 스크롤 최상단으로 이동
-              }
-              .refreshable {
-                viewStore.send(.refreshAPI) // Pull to Refresh
-              }
+            }
+            .simultaneousGesture(DragGesture().onChanged { _ in
+              // ScrollView에서 Drag 시 Keyboard 내리기
+              UIApplication.shared.endEditing()
+            })
+            .onChange(of: store.searchText) { _ in
+              proxy.scrollTo(0, anchor: .top) // 스크롤 최상단으로 이동
+            }
+            .onChange(of: store.selectedTab) { _ in
+              proxy.scrollTo(0, anchor: .top) // 스크롤 최상단으로 이동
+            }
+            .refreshable {
+              store.send(.refreshAPI) // Pull to Refresh
             }
           }
         }
       }
-      .onTapGesture {
-        UIApplication.shared.endEditing()
-      }
+    }
+    .onTapGesture {
+      UIApplication.shared.endEditing()
     }
   }
 }
--- SwiftUI+TCA+Combine/View/Profile/ProfileView.swift
+++ SwiftUI+TCA+Combine/View/Profile/ProfileView.swift
@@ -18,73 +18,71 @@ struct ProfileView: View {
   @State private var showingWebviewSheet = false
   
   var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
-      WithPerceptionTracking {
-        VStack(alignment: .leading, spacing: 0) {
-          Text(viewStore.profile.initial)
-            .font(.system(size: 17.0, weight: .bold))
-            .foregroundColor(.black)
-            .padding(EdgeInsets(top: 10, leading: 15, bottom: 0, trailing: 15))
-            .frame(maxWidth: Screen.width, alignment: .leading)
-            .background(
-              GeometryReader { geometry in
-                Color.clear
-                  .onAppear {
-                    if viewStore.profile.initial == "" {
-                      initialHeight = geometry.size.height
-                      viewHeight -= initialHeight
-                    }
-                    print("geometry: \(geometry.size.height), viewHeight: \(viewHeight)")
+    WithPerceptionTracking {
+      VStack(alignment: .leading, spacing: 0) {
+        Text(store.profile.initial)
+          .font(.system(size: 17.0, weight: .bold))
+          .foregroundColor(.black)
+          .padding(EdgeInsets(top: 10, leading: 15, bottom: 0, trailing: 15))
+          .frame(maxWidth: Screen.width, alignment: .leading)
+          .background(
+            GeometryReader { geometry in
+              Color.clear
+                .onAppear {
+                  if store.profile.initial == "" {
+                    initialHeight = geometry.size.height
+                    viewHeight -= initialHeight
                   }
-              }
-            )
-          
-          HStack(alignment: .center, spacing: 0) {
-            if let imageUrl = viewStore.profile.profileURL {
-              KFImage(URL(string: imageUrl))
-                .resizable()
-                .scaledToFill()
-                .clipShape(Circle())
-                .overlay(Circle().stroke(.black, lineWidth: 1))
-                .frame(width: imageSize, height: imageSize)
-            } else {
-              Image(systemName: "person.circle.fill")
-                .resizable()
-                .frame(width: imageSize, height: imageSize)
-                .foregroundStyle(.gray)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(.black, lineWidth: 1))
+                  print("geometry: \(geometry.size.height), viewHeight: \(viewHeight)")
+                }
             }
-            
-            Text(viewStore.profile.userName ?? "")
-              .font(.system(size: 17.0))
-              .foregroundColor(.black)
-              .lineLimit(2)
-              .padding(EdgeInsets(top: 0, leading: 19, bottom: 0, trailing: 15))
-              .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-              .contentShape(Rectangle())
-            
-            Image(systemName: viewStore.profile.isFavorite ? "star.fill" : "star")
-              .foregroundStyle(.blue)
-              .frame(width: starSize, height: starSize)
-              .contentShape(Rectangle())
-              .onTapGesture {
-                viewStore.send(.favoriteButtonTapped)
-              }
+          )
+        
+        HStack(alignment: .center, spacing: 0) {
+          if let imageUrl = store.profile.profileURL {
+            KFImage(URL(string: imageUrl))
+              .resizable()
+              .scaledToFill()
+              .clipShape(Circle())
+              .overlay(Circle().stroke(.black, lineWidth: 1))
+              .frame(width: imageSize, height: imageSize)
+          } else {
+            Image(systemName: "person.circle.fill")
+              .resizable()
+              .frame(width: imageSize, height: imageSize)
+              .foregroundStyle(.gray)
+              .clipShape(Circle())
+              .overlay(Circle().stroke(.black, lineWidth: 1))
           }
-          .padding(.horizontal, 15)
-          .frame(width: Screen.width, alignment: .leading)
-          .frame(maxHeight: .infinity, alignment: .leading)
-          .onTapGesture {
-            showingWebviewSheet.toggle()
-          }
-          .sheet(isPresented: $showingWebviewSheet) {
-            RepoWebView(url: viewStore.profile.repositoryURL)
-              .presentationDragIndicator(.visible)
-          }
+          
+          Text(store.profile.userName ?? "")
+            .font(.system(size: 17.0))
+            .foregroundColor(.black)
+            .lineLimit(2)
+            .padding(EdgeInsets(top: 0, leading: 19, bottom: 0, trailing: 15))
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            .contentShape(Rectangle())
+          
+          Image(systemName: store.profile.isFavorite ? "star.fill" : "star")
+            .foregroundStyle(.blue)
+            .frame(width: starSize, height: starSize)
+            .contentShape(Rectangle())
+            .onTapGesture {
+              store.send(.favoriteButtonTapped)
+            }
         }
-        .frame(width: Screen.width, height: viewHeight)
+        .padding(.horizontal, 15)
+        .frame(width: Screen.width, alignment: .leading)
+        .frame(maxHeight: .infinity, alignment: .leading)
+        .onTapGesture {
+          showingWebviewSheet.toggle()
+        }
+        .sheet(isPresented: $showingWebviewSheet) {
+          RepoWebView(url: store.profile.repositoryURL)
+            .presentationDragIndicator(.visible)
+        }
       }
+      .frame(width: Screen.width, height: viewHeight)
     }
   }
 }
